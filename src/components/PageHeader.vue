6c245f3c33b82bbbeed824447be21be7ad47e762
nqq12345 <598519655@qq.com>
2016-10-27 16:06:41 +0800

    add active class at header

--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -4,7 +4,9 @@
       <span class="logo">Zero</span>
     </div>
     <div class="route-links">
-      <a @click="change" v-for="arr of array">{{ arr }}</a>
+      <a @click="change(index)"
+         v-for="(header, index) in headers"
+         :class="{ active: header.active }">{{ header.name }}</a>
     </div>
     <div class="build-framework">
       <span>Built with Vue 2.0</span>
@@ -13,25 +15,37 @@
 </template>
 
 <script type="text/ecmascript-6">
-export default {
-  name: 'pageHeader',
-  data: function () {
-    return {
-      array: ['Top', 'New', 'Show', 'Ask', 'Job']
-    }
-  },
-  methods: {
-    change: function (event) {
-      let type = event.target.innerHTML.toLowerCase()
-      this.$emit('headerChange', type)
+  export default {
+    name: 'pageHeader',
+    data: function () {
+      return {
+        items: {
+          array: ['top', 'new', 'show', 'ask', 'job'],
+          activeIndex: 0
+        }
+      }
+    },
+    computed: {
+      headers: function () {
+        return this.items.array.map((arr, index) => ({
+          name: arr,
+          active: index === this.items.activeIndex
+        }))
+      }
+    },
+    methods: {
+      change: function (index) {
+        let type = this.items.array[index]
+        this.items.activeIndex = index
+        this.$emit('headerChange', type.toLowerCase())
+      }
     }
   }
-}
 </script>
 
 <style>
 
-  .hc{
+  .hc {
     background-color: #F5A623;
     padding: 4px 40px;
     position: fixed;
@@ -41,31 +55,36 @@ export default {
     right: 0;
   }
 
-  .hc div{
+  .hc div {
     display: inline-block;
     line-height: 50px;
     height: 50px;
     color: #ffffff;
   }
 
-  .logo{
+  .logo {
     border: 1px solid #ffffff;
     padding: 4px;
   }
 
-  .route-links{
+  .route-links {
     margin-left: 40px;
   }
 
-  .route-links a{
+  .route-links a {
     margin: 0 20px;
+    text-transform: uppercase;
   }
 
-  .route-links a:hover{
+  .route-links a.active {
+    border-bottom: 1px solid #ffffff;
+  }
+
+  .route-links a:hover {
     cursor: pointer;
   }
 
-  .build-framework{
+  .build-framework {
     float: right;
     margin-right: 40px;
   }
